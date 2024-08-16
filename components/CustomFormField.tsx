445b3b42c910f2b601895de2e2e0ee8a3fab3edf
Vineet <infinite2202@gmail.com>
2024-08-16 10:55:34 +0530

    textarea

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -21,6 +21,7 @@ import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
+import { Textarea } from "./ui/textarea";
 
 interface CustomProps {
   control: Control<any>;
@@ -71,6 +72,13 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
           </FormControl>
         </div>
       );
+
+    case FormFieldType.TEXTAREA:
+      return(
+        <FormControl>
+          <Textarea placeholder={placeholder} {...field} className="shad-textArea" disabled={props.disabled} />
+        </FormControl>
+      )
 
     case FormFieldType.PHONE_INPUT:
       return (
