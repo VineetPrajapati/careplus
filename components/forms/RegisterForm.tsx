87bb71136a56c574c884925a87ed6de279608f43
Vineet <infinite2202@gmail.com>
2024-08-16 14:38:46 +0530

    registration form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -16,14 +16,19 @@ import {
 
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
-import { UserFormValidation } from "@/lib/validation";
+import { PatientFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.actions";
+import { createUser, registerPatient } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
+import {
+  Doctors,
+  GenderOptions,
+  IdentificationTypes,
+  PatientFormDefaultValues,
+} from "@/constants";
 import { Label } from "../ui/label";
-import { SelectContent, SelectItem } from "../ui/select";
+import { SelectItem } from "../ui/select";
 import Image from "next/image";
 import CustomFormField from "../CustomFormField";
 import FileUploader from "../FileUploader";
@@ -34,34 +39,64 @@ const RegisterForm = ({ user }: { user: User }) => {
   const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof UserFormValidation>>({
-    resolver: zodResolver(UserFormValidation),
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
     defaultValues: {
-      name: "",
-      email: "",
-      phone: "",
+      ...PatientFormDefaultValues,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
     },
   });
 
   // 2. Define a submit handler.
-  async function onSubmit({
-    name,
-    email,
-    phone,
-  }: z.infer<typeof UserFormValidation>) {
+  async function onSubmit(values: z.infer<typeof PatientFormValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     setIsLoading(true);
 
+    let formData;
+
+    if (
+      values.identificationDocument &&
+      values.identificationDocument.length > 0
+    ) {
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      });
+
+      formData = new FormData();
+      formData.append("blobFile", blobFile);
+      formData.append("fileName", values.identificationDocument[0].name);
+    }
+
     try {
-      const userData = { name, email, phone };
+      const patientData = {
+        ...values,
+        userId: user.$id,
+        birthDate: new Date(values.birthDate),
+        identificationDocument: formData,
+      };
 
-      const user = await createUser(userData);
+      // @ts-ignore
+      const patient = await registerPatient(patientData);
 
-      if (user) router.push(`/patients/${user.$id}/register`);
+      if (patient) router.push(`/patients/${user.$id}/new-appointment`);
     } catch (error) {
       console.log(error);
     }
+
+    setIsLoading(false);
+
+    // try {
+    //   const userData = { name, email, phone };
+
+    //   const user = await createUser(userData);
+
+    //   if (user) router.push(`/patients/${user.$id}/register`);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 
   return (
@@ -295,7 +330,7 @@ const RegisterForm = ({ user }: { user: User }) => {
           placeholder="Select an identification type"
         >
           {IdentificationTypes.map((type) => (
-            <SelectItem key={type} value={type}>
+            <SelectItem key={type} value={type} className="cursor-pointer">
               {type}
             </SelectItem>
           ))}
