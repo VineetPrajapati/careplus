e6911ce578ad233fe48267d0d634dfc27c770255
Vineet <infinite2202@gmail.com>
2024-08-17 11:52:46 +0530

    get appointment

--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -1,22 +1,40 @@
-"use server"
+"use server";
 
 import { ID } from "node-appwrite";
-import { APPOINTMENT_COLLECTION_ID, databases, DATABSE_ID } from "../appwrite.config";
+import {
+  APPOINTMENT_COLLECTION_ID,
+  databases,
+  DATABSE_ID,
+} from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-export const  createAppointment = async (appointment:  CreateAppointmentParams) => {
-    try {
-        const newAppointment = await databases.createDocument(
-            DATABSE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            ID.unique(),
-            appointment,
-          );
-    
-          return parseStringify(newAppointment);
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    const newAppointment = await databases.createDocument(
+      DATABSE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      ID.unique(),
+      appointment
+    );
+
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABSE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
